f404c52ff7d10da91668a546b78b9dd777138b1b
Yura Volokitin <yuri.volokitin@gmail.com>
2020-01-14 20:40:43 +0100

    added flask

--- supermath.py
+++ supermath.py
@@ -1,8 +1,10 @@
 ﻿#!flask/bin/python
 import os
 from flask import Flask, abort, request, jsonify, g, url_for
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
