3d2b15a7100413fc9ba77d00ed1fbe99dd6e0729
Yura Volokitin <yuri.volokitin@gmail.com>
2020-05-07 18:13:08 +0200

    added sql helper

---
+++ sql_scripts/halpers.sql
@@ -0,0 +1,5 @@
+﻿select ID, NAME, FAIL from users;
+
+select ID, NAME, SURNAME, AVATAR, FAIL from users ORDER BY FAIL DESC LIMIT 10;
+
+select ID, NAME, SURNAME, AVATAR, PASS from users ORDER BY PASS DESC LIMIT 10;
